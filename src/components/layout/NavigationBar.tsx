dff461d3f5da15abf416c4ac7693f50183964873
maxim-dirtu <maxim.dirtu@gmail.com>
2026-01-14 22:47:38 +0200

    added logo

--- src/components/layout/NavigationBar.tsx
+++ src/components/layout/NavigationBar.tsx
@@ -4,8 +4,10 @@ import navbarResources from '@/lib/resources/navbarResources';
 export default function NavigationBar() {
   return (
     <nav className="bg-neutral-950 text-white shadow-lg">
-      <div className="container mx-auto px-4 py-4 flex justify-between items-center">
-        <div className="text-2xl font-bold">CinemaPlatform</div>
+      <div className="container mx-auto px-4 flex justify-between items-center">
+        <div className="flex items-center">
+          <img src="/logo.png" alt="CinemaPlatform Logo" className="w-auto pb-2" />
+        </div>
         <div className="flex gap-8 mr-6">
           <Link href="/" className="hover:text-gray-300 transition-colors">
             {navbarResources.homepage}
